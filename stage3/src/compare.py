cd8856530e5e166405946d5d9a5d976f95c107fc
zaki <goibagabba@gmail.com>
2026-03-10 15:40:27 -0500

    path logic fix again

--- stage3/src/compare.py
+++ stage3/src/compare.py
@@ -15,7 +15,7 @@ out_path = dspath / out_folder
 con_path = out_path / "con_channel_band_avg.csv"
 alz_path = out_path / "alz_channel_band_avg.csv"
 
-if not (con_path.exists() and alz_path.exists()) :
+if not con_path.exists() or not alz_path.exists() :
     raise Exception("No .csv files found! Run datagen.py before running this script!")
 
 print(f"'out' folder found at: {out_path}")
