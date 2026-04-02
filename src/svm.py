35a67e10ead4ce9c838cd7627e9cb05c4c341775
zafir99 <zafiruahmed@gmail.com>
2026-04-02 16:47:38 -0500

    bad loso implementation

--- src/svm.py
+++ src/svm.py
@@ -33,55 +33,64 @@ gamma = (25,45)
 freq_bands = (delta, theta, alpha, beta, gamma)
 num_bands = len(freq_bands)
 
-alz_list = []
 
 skip = 0
-for i in range (num_a) :
-    if (i == skip) :
-        continue
-    csv = pd.read_csv(alz_root / ("sub_" + str(i+alz_index[0]).zfill(3) + "_rbp.csv"), sep=',')
-    alz_list.append(csv)
-
-alz_data = pd.concat(alz_list).to_numpy(dtype=np.float64)
-del alz_list
-
-con_list = []
-for i in range (num_c) :
-    if (i == skip) :
-        continue
-    csv = pd.read_csv(con_root / ("sub_" + str(i+con_index[0]).zfill(3) + "_rbp.csv"), sep=',')
-    con_list.append(csv)
-
-reserved_con = pd.read_csv(con_root / ("sub_" + str(skip+con_index[0]).zfill(3) + "_rbp.csv"), sep=',').to_numpy(dtype=np.float64)
-reserved_alz = pd.read_csv(alz_root / ("sub_" + str(skip+alz_index[0]).zfill(3) + "_rbp.csv"), sep=',').to_numpy(dtype=np.float64)
-
-con_data = pd.concat(con_list).to_numpy(dtype=np.float64)
-del con_list
-
-count_a_recs = alz_data.shape[0]
-count_c_recs = con_data.shape[0]
-total_data = np.concat([con_data, alz_data], axis=0)
-
-del alz_data
-del con_data
-
-labels = ["control"] * count_c_recs + ["alzheimers"] * count_a_recs
-bin_labels = [0] * count_c_recs + [1] * count_a_recs
-
-clf = svm.SVC(kernel='poly', degree=5)
-clf.fit(total_data,bin_labels)
-lab_arr = clf.predict(reserved_con)
-
+skip_root = alz_root
 acc = 0
-for num in lab_arr :
-    acc += 1-num
-acc /= len(lab_arr)
-print(f"Control Accuracy Rate: {acc*100}")
+total_rec = 0
 
-acc = 0
-lab_arr = clf.predict(reserved_alz)
-for num in lab_arr:
-    acc += num
+for n in range(con_index[1]) :
+    skip = n + alz_index[0]
+    if (skip >= con_index[0]) :
+        skip_root = con_root
 
-acc /= len(lab_arr)
-print(f"Alzheimers Accuracy Rate: {acc*100}")
+    alz_list = []
+    for i in range (num_a) :
+        idx = i+alz_index[0]
+        if (idx == skip) :
+            continue
+        sub_file = "sub_" + str(idx).zfill(3) + "_rbp.csv"
+        csv = pd.read_csv(alz_root / sub_file, sep=',')
+        alz_list.append(csv)
+
+    alz_data = pd.concat(alz_list).to_numpy(dtype=np.float64)
+    del alz_list
+
+    con_list = []
+    for i in range (num_c) :
+        idx = i + con_index[0]
+        if (idx == skip) :
+            continue
+        sub_file = "sub_" + str(idx).zfill(3) + "_rbp.csv"
+        csv = pd.read_csv(con_root / ("sub_" + str(idx).zfill(3) + "_rbp.csv"), sep=',')
+        con_list.append(csv)
+
+    test = pd.read_csv(skip_root / ("sub_" + str(skip).zfill(3) + "_rbp.csv"), sep=',').to_numpy(dtype=np.float64)
+
+    con_data = pd.concat(con_list).to_numpy(dtype=np.float64)
+    del con_list
+
+    count_a_recs = alz_data.shape[0]
+    count_c_recs = con_data.shape[0]
+    total_data = np.concat([con_data, alz_data], axis=0)
+
+    del alz_data
+    del con_data
+
+    labels = ["control"] * count_c_recs + ["alzheimers"] * count_a_recs
+    bin_labels = [0] * count_c_recs + [1] * count_a_recs
+    expected = 0
+    if (skip <= alz_index[1]) :
+        expected = 1
+
+    clf = svm.SVC(kernel='poly', degree=5)
+    clf.fit(total_data,bin_labels)
+
+    print(f"Testing subject {skip}'s data...")
+    lab_arr = clf.predict(test)
+
+    total_rec += len(lab_arr)
+    acc += lab_arr.tolist().count(expected)
+
+acc /= total_rec
+print(f"Accuracy Rate: {acc*100}%")
