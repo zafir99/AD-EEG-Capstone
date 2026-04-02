14b62c7576e5f2d691463d6b6e9544c7d177461b
zafir99 <zafiruahmed@gmail.com>
2026-04-01 22:37:08 -0500

    scuffed

--- src/svm.py
+++ src/svm.py
@@ -1,4 +1,5 @@
-import sklearn
+from sklearn import svm
+import pandas as pd
 import numpy as np
 from os import getcwd, makedirs, execv
 from pathlib import Path
@@ -22,3 +23,45 @@ num_a = alz_index[1]-alz_index[0]+1
 num_c = con_index[1]-con_index[0]+1
 num_f = ftd_index[1]-ftd_index[0]+1
 num_subjects = 88
+
+delta = (0.5,4)
+theta = (4,8)
+alpha = (8,13)
+beta  = (13,30)
+gamma = (30,45)
+freq_bands = (delta, theta, alpha, beta, gamma)
+num_bands = len(freq_bands)
+
+alz_list = []
+
+for i in range (num_a-1) :
+    csv = pd.read_csv(alz_root / ("sub_" + str(i+alz_index[0]).zfill(3) + "_rbp.csv"), sep=',')
+    alz_list.append(csv)
+
+alz_data = pd.concat(alz_list).to_numpy(dtype=np.float64)
+del alz_list
+
+con_list = []
+for i in range (num_c-1) :
+    csv = pd.read_csv(con_root / ("sub_" + str(i+con_index[0]).zfill(3) + "_rbp.csv"), sep=',')
+    con_list.append(csv)
+
+reserved_alz = pd.read_csv(alz_root / ("sub_" + str(alz_index[1]).zfill(3) + "_rbp.csv"), sep=',').to_numpy(dtype=np.float64)
+reserved_con = pd.read_csv(con_root / ("sub_" + str(con_index[1]).zfill(3) + "_rbp.csv"), sep=',').to_numpy(dtype=np.float64)
+
+con_data = pd.concat(con_list).to_numpy(dtype=np.float64)
+del con_list
+
+count_a_recs = alz_data.shape[0]
+count_c_recs = con_data.shape[0]
+total_data = np.concat([con_data, alz_data], axis=0)
+
+#del alz_data
+#del con_data
+
+labels = ["control"] * count_c_recs + ["alzheimers"] * count_a_recs
+int_labels = [0] * count_c_recs + [1] * count_a_recs
+clf = svm.SVC(kernel='poly')
+clf.fit(total_data,labels)
+arr = clf.predict(reserved_alz)
+
